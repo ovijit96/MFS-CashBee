18ca0882aa836eb3ee60c4d2962e76c6d3380618
ovijit96 <ovijitdeykumar@gmail.com>
2025-08-23 01:03:20 +0600

    login btn test ok

---
+++ script.js
@@ -0,0 +1,20 @@
+// loginButton functionality
+document.getElementById("loginButton").addEventListener("click",function(e){
+    e.preventDefault()
+    const mobileNumber = 1632750245;
+    const pinNumber = 99566;
+    const mobileNumbervalue = document.getElementById("mobile-number").value
+    const mobileNumbervalueConverted = parseInt(mobileNumbervalue)
+
+    const pinNumberValue = document.getElementById("pin-number").value
+    const pinNumberValueConverted = parseInt(pinNumberValue)
+
+    console.log(mobileNumbervalueConverted, pinNumberValueConverted);
+
+    if(mobileNumbervalueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+        console.log("all value match")
+    }
+    else{
+        console.log("Invalid credettial")
+    }
+})
